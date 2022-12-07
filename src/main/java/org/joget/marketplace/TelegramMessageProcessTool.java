68ad2106692c758f36511acb55ec3587d682407e
DonutEmperor <56782553+DonutEmperor@users.noreply.github.com>
2022-12-07 21:46:56 +0800

    Initial commit

---
+++ src/main/java/org/joget/marketplace/TelegramMessageProcessTool.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.joget.marketplace;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Map;
+import org.joget.apps.app.service.AppPluginUtil;
+import org.joget.apps.app.service.AppUtil;
+import org.joget.commons.util.LogUtil;
+import org.joget.plugin.base.DefaultApplicationPlugin;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class TelegramMessageProcessTool extends DefaultApplicationPlugin{
+    
+    private final static String MESSAGE_PATH = "message/form/TelegramMessageProcessTool";
+    @Override
+    public String getName() {
+        return "Telegram Message Process Tool";
+    }
+
+    @Override
+    public String getVersion() {
+        return "8.0.0";
+    }
+
+    @Override
+    public String getDescription() {
+        return AppPluginUtil.getMessage("org.joget.marketplace.TelegramMessageProcessTool.pluginDesc" , getClassName() , MESSAGE_PATH);
+    }
+
+    @Override
+    public String getLabel() {
+        return AppPluginUtil.getMessage("org.joget.marketplace.TelegramMessageProcessTool.pluginLabel", getClassName(), MESSAGE_PATH);
+    }
+
+    @Override
+    public String getClassName() {
+        return getClass().getName();
+    }
+
+    @Override
+    public String getPropertyOptions() {
+        return AppUtil.readPluginResource(getClassName(), "/properties/form/telegramMessageProcessTool.json", null, true, MESSAGE_PATH);
+    }
+
+        
+    @Override
+    public Object execute(Map props){
+        
+        //The telegram url
+        String url = "https://api.telegram.org/bot";
+        
+       //Get declared properties value 
+        String botToken = getPropertyString("botToken");
+        String userName = getPropertyString("userName");
+        String groupId = getPropertyString("groupId");
+        String message = getPropertyString("message");
+        String header = getPropertyString("header");
+        
+        //The url concatenated
+        String theUrl = url + botToken + "/" + "sendMessage?chat_id=" + groupId + "&" + "parse_mode=HTML&text=<b>" + header + "</b>%0A" + message + "%0ABy%0A<b>" + userName + "</b>";  
+        
+        
+        //Make connection request to url
+        try{
+            URL result = new URL(theUrl);
+            URLConnection conn = result.openConnection();
+            conn.connect();
+            InputStream is = new BufferedInputStream(conn.getInputStream());
+        }catch(Exception e){
+            LogUtil.error(this.getClassName(), e, "Fail to send Message");
+        }
+        
+        return null;
+    }
+    
+}   
